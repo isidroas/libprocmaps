b051d2063fa1a77ed5f95be7f6166a4f506fab32
Isidro Arias <isidroariass@hotmail.es>
2024-10-14 10:47:14 +0200

    added test array

--- tests/test_parsing.c
+++ tests/test_parsing.c
@@ -12,7 +12,22 @@
 #include <stddef.h>
 #include <stdio.h>
 
-int main(void)
+void test_make_procmaps_array()
+{
+    char contents[] =
+        "00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon\n"
+        "00651000-00652000 r--p 00051000 08:02 173521      /usr/bin/dbus-daemon\n";
+    FILE *stream = fmemopen(contents, sizeof(contents)-1 /* remove null byte */, "r");
+    hr_procmaps **procmaps_array = make_procmaps_array(stream);
+    assert(procmaps_array[0]);
+    assert(procmaps_array[0]->addr_begin == 0x00400000);
+    assert(procmaps_array[1]);
+    assert(procmaps_array[1]->addr_begin == 0x00651000);
+    assert(procmaps_array[2]==NULL);
+    destroy_procmaps(procmaps_array);
+}
+
+void test_parse_line()
 {
     char *mapstr = strdup("00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon\n\0");
     char permissions = ((0 | PERMS_READ) | PERMS_EXECUTE) | PERMS_PRIVATE;
@@ -30,6 +45,12 @@ int main(void)
     free(mapstr);
     free(map->pathname);
     free(map);
+}
+
+int main(void)
+{
+    test_parse_line();
+    test_make_procmaps_array();
     printf("All tests passed :)\n");
     return 0;
 }
